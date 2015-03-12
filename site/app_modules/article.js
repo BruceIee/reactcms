6ab08979405262609b8f0134e9b870228cfd7bc6
ljnet <ljnet.mail@gmail.com>
2015-03-12 23:48:45 +0800

    adding articles, test1

---
+++ site/app_modules/article.js
@@ -0,0 +1,86 @@
+var util = require('util');
+var tool = require('leaptool');
+
+module.exports = function(app) {
+    
+    var moduleName = 'article';
+    var block = {
+        app: app,
+        model: null
+    };
+    block.data = tool.object(require('basedata')(app, moduleName));
+    block.page = tool.object(require('basepage')(app, moduleName, block.data));
+    
+    block.model = {
+        type: {
+            type: 'string'
+        },
+        title: {
+            type: 'string'
+        },
+        content: {
+            type: 'string'
+        },
+        data: {
+            type: 'object',
+            subtype: {
+                type: 'json'
+            }
+        },
+        status: {
+            type: 'string'
+        },
+        create_date: {
+            type: 'date'
+        }
+    };
+    
+    
+    block.page.addArticle = function(req, res) {
+        var page = app.getPage(req);
+        page.title = 'Add an article';
+        res.render('article/add_article', { page:page });
+    };
+
+    
+    
+    block.data.addItem = function(req, res) {
+        var callback = arguments[3] || null; 
+        var item = tool.getReqParameter(req);
+        item.create_date = new Date();
+        block.data.add(req, res, item, function(error, docs, info) {
+            app.cb(error, docs, info, req, res, callback);
+        });
+    };
+    
+    
+    block.data.articlePost = function(req, res) {
+        var callback = arguments[3] || null; 
+        var article = tool.getReqParameter(req);
+        article.create_date = new Date();
+        console.log('article=',article);
+        block.data.add(req, res, article, function(error, docs, info) {
+            app.cb(error, docs, info, req, res, callback);
+        });
+    };
+    
+    
+    
+    
+    // data route
+    
+    //app.server.get('/data/item/add', block.data.addItem);
+    //app.server.post('/data/item/add', block.data.addItem);
+    
+    app.server.post('/data/article/article_post', block.data.articlePost);
+    
+    
+    // page route
+    //app.server.get('/item', block.page.getIndex);
+    
+    app.server.get('/' + moduleName + '/add', block.page.addArticle);
+    
+
+    return block;
+};
+
