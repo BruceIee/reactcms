94cb39bc5432a4a0a7d9ff3b5583be66cf00064b
yayestechlab <yayestechlab@me.com>
2015-03-13 20:24:45 -0400

    announcement should be working

--- site/app_modules/announcement.js
+++ site/app_modules/announcement.js
@@ -33,15 +33,33 @@ module.exports = function(app) {
             app.cb(error, docs, info, req, res, callback);
         });
     };
+    block.page.getAnnouncementIndex = function(req, res) {
+        console.log('---------');
+        var parameter = tool.getReqParameter(req);
+        console.log(parameter);
+        var condition = {};
+        var filter = {};
+
+        app.db.find(moduleName, condition, filter, function(error, docs, info){
+            console.log('error=',error);
+            console.log('docs=',docs);
+            console.log('info=',info);
+
+            var page = app.getPage(req);
+            //page.id = parameter.id;
+            page.title = 'Announcements';
+            page.announcements = docs;
+            console.log('page=',page);
+            res.render('announcements/index', { page:page });
+        });
+    };
     
     // data route
     app.server.get('/data/announcement/add', block.data.addItem);
     app.server.post('/data/announcements/add', block.data.addItem);
     // page route
-    app.server.get('/announcements', block.page.getIndex);
-    //TODO: ask Yue about using id's in node/mongo.
-    //TODO: ask about adding more routes.
-    // app.server.get('/announcement/:id? or :url', block.page.getIndex);
+    app.server.get('/announcements', block.page.getAnnouncementIndex);
+    app.server.get('/announcement/:_id', block.data.addItem);
 
     return block;
 };
