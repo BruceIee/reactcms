f6c791562cbbc5c93c6f5b2a8cfd92ee1d920f9b
yayestechlab <yayestechlab@me.com>
2015-03-15 13:22:32 -0400

    truncation working

--- site/app_modules/announcement.js
+++ site/app_modules/announcement.js
@@ -50,6 +50,13 @@ module.exports = function(app) {
             docs.reverse();
             page.announcements = docs;
             page.controller = 'announcements';
+            page.shorten = function(text) {
+                var ret = text;
+                if (ret.length > 100) {
+                    ret = ret.substr(0,100-3) + "&hellip;";
+                }
+                return ret;
+            };
             console.log('page=',page);
             res.render('announcement/index', { page:page });
         });
