7f5896ae7a623fb33d7d322ac3bd8027450a6716
yliu0 <uestudio@gmail.com>
2015-03-28 12:08:40 -0400

    item detail component update

--- site/public/script/item/detail_react.js
+++ site/public/script/item/detail_react.js
@@ -8,39 +8,17 @@ $().ready(function() {
 });
 
 function getItemDetail(itemId, callback) {
-    
     var itemDetailUrl = '/data/items/' + itemId + '/detail';
     $.get(itemDetailUrl, function(data) {
-        //console.log('item detail:', itemId, data);
         var item =  data.docs && data.docs[0] || null;
+        item.id = item._id + '';
         callback && callback(item);
-    });
-    
-    var itemsUrl = '/data/items';
-    $.get(itemsUrl, function(data) {
-        var items = data.docs;
-        _.map(items, function(item) {
-            item['id'] = item._id;
-            item['text'] = item.name;
-            item['iconClass'] = 'fa fa-fw fa-book';
-            return item;
-        });
-        updateItemList(items);
     });
 }
 
 function showItemDetail(item) {
-    console.log('>>> showItemDetail:', item);
-}
-
-function updateItemList(items) {
-    app.listData = { items: items };
     app.itemDetail = React.render(
-        <ItemDetail data={ app.listData } />,
+        <ItemDetail data={ item } />,
         document.getElementById('itemDetail')
     );
-    app.itemDetail.on('select', function(id) {
-        var itemUrl = '/items/' + id + '/detail/react';
-        window.location = itemUrl;
-    });
 }
