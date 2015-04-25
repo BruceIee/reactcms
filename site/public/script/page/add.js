dd092b5fd68eda190c61085e114547e79e152e26
leapon <uestudio@gmail.com>
2015-04-25 00:48:19 -0400

    add page update

--- site/public/script/page/add.js
+++ site/public/script/page/add.js
@@ -1,15 +1,27 @@
 var app = app || {};
+app.compositionCol = {}; // composition collection keyed by composition name
 
 $().ready(function() {
     setup();
 });
 
 function setup() {
-    console.log('in page add script');
-
     // setup composition dropdown combobox
     var compositionListUrl = '/data/compositions';
-    
-    
+    $.get(compositionListUrl, function(data) {
+        var compositions = data.docs;
+        for (var i = 0; i < compositions.length; i++) {
+            var composition = compositions[i];
+            //console.log('>>> composition:', composition);
+            app.compositionCol[composition.name] = composition;
+        }
+        setupCompositionSelect();
+    });
 }
 
+function setupCompositionSelect() {
+    var compositionSelect = $('.composition-select');
+    for (var name in app.compositionCol) {
+        compositionSelect.append('<option>' + name + '</option>');
+    }
+}
