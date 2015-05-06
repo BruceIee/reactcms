cd149de5541e12eab81954890795a33164a1dc7d
leapon <uestudio@gmail.com>
2015-05-05 20:51:15 -0400

    move component rendering to js file

--- site/public/script/composition/sidenav.js
+++ site/public/script/composition/sidenav.js
@@ -1,7 +1,44 @@
+var app = app || {};
+
+app.widgets = {
+    ItemDetail: ItemDetail,
+    ArticleDetail: ArticleDetail,
+    LinksetDetail: LinksetDetail,
+    LinksetDetail2: LinksetDetail2
+};
+
 $().ready(function() {
-    setup();
+    //console.log('page content:', app.pageContent);
+    //console.log('page composition:', app.pageComposition);
+    for (var targetClass in app.pageContent) {
+        var widgets = app.pageContent[targetClass];
+        for (var i = 0; i < widgets.length; i++) {
+            var widget = widgets[i];
+            widget.targetClass = targetClass;
+            showWidget(widget);
+        }
+    }
 });
 
-function setup() {
-    console.log('in composition sidenav page');
+function showWidget(widget) {
+    //console.log('showWidget on targetClass:', widget.targetClass);
+    //console.log('widget name:', widget.widgetName);
+    //console.log('widget info:', widget.widgetInfo);
+    //$('.' + widget.targetClass).append(widget.widgetName);
+    var widgetDetailUrl = '/data/components/get/detail';
+    $.get(widgetDetailUrl, widget, function(data) {
+        console.log('data.docs.length:', data.docs.length);
+        if (data.docs.length <= 0) return;
+        
+        var cabinetData = { items: [] };
+        for (var i = 0; i < data.docs.length; i++) {
+            var widgetData = data.docs && data.docs[i];
+            cabinetData.items.push({ type:this.widgetName, data:widgetData });
+        }
+        
+        React.render(
+            <Cabinet data={ cabinetData } />,
+            $('.' + this.targetClass)[0]
+        );
+    }.bind(widget));
 }
