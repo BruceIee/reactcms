c185c0896f583962f61abcff3af5d0561d0b8e5a
leapon <uestudio@gmail.com>
2015-05-23 11:41:27 -0400

    added url to get module model

--- site/app_modules/admin.js
+++ site/app_modules/admin.js
@@ -11,6 +11,16 @@ module.exports = function(app) {
     block.data = tool.object(require('basedata')(app, moduleName));
     block.page = tool.object(require('basepage')(app, moduleName, block.data));
     
+    // data
+    block.data.getModuleModel = function(req, res) {
+        var callback = arguments[3] || null; 
+        var parameter = tool.getReqParameter(req);
+        var moduleName = parameter.module;
+        var moduleModel = app.module[moduleName] && app.module[moduleName].model || null;
+        var info = { message:'model for module ' + moduleName };
+        app.cb(null, moduleModel, info, req, res, callback);
+    };
+    
     // page
     block.page.getIndex = function(req, res) {
         var page = app.getPage(req);
@@ -26,11 +36,16 @@ module.exports = function(app) {
     };
     
     // routes
+    app.server.all('/data/module/*', block.page.checkLogin);
+    app.server.get('/data/module/:module/model', block.data.getModuleModel);
+    
+    app.server.all('/module/*', block.page.checkLogin);
+    app.server.get('/module/:module/list', block.page.getCommonListPage);
+    
     app.server.all('/admin', block.page.checkLogin);
     app.server.all('/admin/*', block.page.checkLogin);
     app.server.get('/admin', block.page.getIndex);
     app.server.get('/' + moduleName + '/page/:pagename', block.page.showPage);
-    app.server.get('/admin/:module/list', block.page.getCommonListPage);
     
     return block;
 };
