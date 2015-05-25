5536cd20fd59eb9d0705884951e752f4d2bd62d0
leapon <uestudio@gmail.com>
2015-05-24 23:28:37 -0400

    update on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var react = require('gulp-react');
+var watchify = require('watchify');
+var reactify = require('reactify');
+
+/*
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 
 var path = {
+  HTML: 'src/index.html',
+  MINIFIED_OUT: 'build.min.js',
+  OUT: 'build.js',
+  DEST: 'dist',
+  DEST_BUILD: 'dist/build',
+  DEST_SRC: 'dist/src',
+  ENTRY_POINT: './src/js/App.js'
+};
+*/
+
+var path = {
+  OUT: 'build.js',
   JS: ['site/public/script/common/src/*.js'],
-  DEST_SRC: 'site/public/script/common/dist'
+  DEST_SRC: 'site/public/script/common/dist',
+  ENTRY_POINT: 'site/public/script/common/dist/list.js'
 };
 
 gulp.task('transform', function(){
@@ -13,6 +38,27 @@ gulp.task('transform', function(){
     .pipe(gulp.dest(path.DEST_SRC));
 });
 
-gulp.task('default',  ['transform']);
+/*
+gulp.task('watch', function() {
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+*/
+
+gulp.task('default', ['transform']);
+//gulp.task('default', ['watch']);
 
 
