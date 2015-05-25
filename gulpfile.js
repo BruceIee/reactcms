36b4360d447fd337fd941fe79cbf3948bdf734ce
leapon <uestudio@gmail.com>
2015-05-24 22:46:59 -0400

    gulp react transform works

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var react = require('gulp-react');
 
 var path = {
-  HTML: 'src/index.html',
-  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
-  JS: ['src/js/*.js', 'src/js/**/*.js'],
-  MINIFIED_OUT: 'build.min.js',
-  DEST_SRC: 'dist/src',
-  DEST_BUILD: 'dist/build',
-  DEST: 'dist'
+  JS: ['site/public/script/common/src/*.js'],
+  DEST_SRC: 'site/public/script/common/dist'
 };
 
-/*
 gulp.task('transform', function(){
   gulp.src(path.JS)
     .pipe(react())
     .pipe(gulp.dest(path.DEST_SRC));
 });
-*/
 
-gulp.task('default', function(){
-    // place code for your default task here
-});
+gulp.task('default',  ['transform']);
 
 
