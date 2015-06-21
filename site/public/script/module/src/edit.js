85c80fdc69bc186a8e3d35379c4bf15223f2c204
leapon <uestudio@gmail.com>
2015-06-21 19:46:02 -0400

    reactlet table test added in edit page

--- site/public/script/module/src/edit.js
+++ site/public/script/module/src/edit.js
@@ -11,7 +11,7 @@ var app = app ||  window.app || {};
 
 $().ready(function() {
     setup();
-    testForm1();
+    testForm3();
 });
 
 function setup() {
@@ -101,6 +101,13 @@ function testForm1() {
         React.createElement('HtmlInput', { data:app.component1Data }),
         document.getElementById('form1')
     );
+    
+    /*
+    app.table1 = React.render(
+        React.createElement(Table, { data:app.table1Data }),
+        document.getElementById('table1')
+    );
+    */
 }
 
 function testForm2() {
@@ -134,4 +141,30 @@ function testForm2() {
         <HtmlForm data={ component2Data } />,
         document.getElementById('form2')
     );
+}
+
+function testForm3() {
+    app.table3Data = {
+        boxClass: 'table-container-bordered',
+        colModel: {
+            id: { name:'id', text:'ID', width:'15%', key:true },
+            name: { name:'name', text:'Name', width:'20%', sort:'up' },
+            price: { name:'price', text:'Price', width:'15%', type:'money' },
+            description: { name:'description', text:'Description', width:'30%' },
+            extra: { name:'extra', text:'Extra', show:false, width:'10%' }
+        },
+        dataItems:[
+            { id:'P01', name:'egg', price:7, description:'fresh egg', extra:'n/a' },
+            { id:'P21', name:'bread', price:2.99, description:'whole grain bread', extra:'n/a' },
+            { id:'P23', name:'chip', price:2.1, description:'potato chip', extra:'n/a' },
+            { id:'P54', name:'sauce', price:1.89, description:'dipping sauce', extra:'n/a' },
+            { id:'P81', name:'corn', price:4.59, description:'fresh corn', extra:'n/a' }
+        ]
+    };
+    
+    app.table3 = React.render(
+        React.createElement(Table, { data:app.table3Data }),
+        document.getElementById('form3')
+    );
+
 }
