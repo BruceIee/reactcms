63acf5f4f88a6db5d4d2aef25d89535a97be016c
leapon <uestudio@gmail.com>
2015-06-24 07:40:47 +0800

    populate create/edit info on module item create/edit

--- site/app_modules/module.js
+++ site/app_modules/module.js
@@ -98,6 +98,7 @@ module.exports = function(app) {
         var moduleData = app.module[moduleName].data;
         //delete parameter.module;
         parameter.create_date = new Date();
+        parameter.create_by = 'admin';
         //console.log('>>> addModuleItem:', moduleName, parameter);
         moduleData.add(req, res, parameter, function(error, docs, info) {
             //console.log('>>> addModuleItem result:', error, docs, info);
@@ -111,7 +112,8 @@ module.exports = function(app) {
         var moduleName = parameter.module;
         var moduleData = app.module[moduleName].data;
         //delete parameter.module;
-        parameter.edit_date = new Date();
+        parameter.edit_date = parameter.edit_date || new Date();
+        parameter.edit_by = parameter.edit_by  || 'admin';
         //console.log('>>> editModuleItem:', moduleName, parameter);
         moduleData.edit(req, res, parameter, function(error, docs, info) {
             //console.log('>>> editModuleItem result:', error, docs, info);
