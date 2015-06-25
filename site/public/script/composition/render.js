81ef4e680a3a5710c076c3433553f713efd126bb
leapon <uestudio@gmail.com>
2015-06-25 20:40:32 +0800

    public/component/graph folder added; Bartest not rendered on page

--- site/public/script/composition/render.js
+++ site/public/script/composition/render.js
@@ -4,12 +4,11 @@ app.widgets = {
     ItemDetail: ItemDetail,
     ArticleDetail: ArticleDetail,
     LinksetDetail: LinksetDetail,
-    LinksetDetail2: LinksetDetail2
+    LinksetDetail2: LinksetDetail2,
+    GraphBartest: GraphBartest
 };
 
 $().ready(function() {
-    //console.log('page content:', app.pageContent);
-    //console.log('page composition:', app.pageComposition);
     for (var targetClass in app.pageContent) {
         var widgets = app.pageContent[targetClass];
         for (var i = 0; i < widgets.length; i++) {
@@ -21,20 +20,14 @@ $().ready(function() {
 });
 
 function showWidget(widget) {
-    //console.log('showWidget on targetClass:', widget.targetClass);
-    //console.log('widget name:', widget.widgetName);
-    //console.log('widget info:', widget.widgetInfo);
-    //$('.' + widget.targetClass).append(widget.widgetName);
     var widgetDetailUrl = '/data/components/get/detail';
     $.get(widgetDetailUrl, widget, function(data) {
-        //console.log('data.docs.length:', data.docs.length, data.docs);
         if (data.docs.length <= 0) return;
         var cabinetData = { items: [] };
         for (var i = 0; i < data.docs.length; i++) {
             var widgetData = data.docs && data.docs[i];
             cabinetData.items.push({ type:this.widgetName, data:widgetData });
         }
-        //console.log('cabinetData:', cabinetData);
         React.render(
             <Cabinet data={ cabinetData } />,
             $('.' + this.targetClass)[0]
