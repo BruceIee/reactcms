7ac23fc8d25f4721072a4a8de2f52a97a9a0968e
leapfish <fish@leapon.com>
2015-08-18 15:51:43 -0400

    new files for fishfooter added

---
+++ site/public/component/footer/fish_footer.js
@@ -0,0 +1,51 @@
+// article detail component
+var FishFooter = React.createClass({
+    name: 'fishfooter',
+    mixins: [getCommonMixin],
+    
+    // attribute definitions
+    getAttributes: function() {
+        var attributes = [
+            { name:'boxClass', type:'string', required:false, defaultValue:'', note:'container CSS class' },
+            { name:'iconClass', type:'string', required:false, defaultValue:'', note:'icon CSS class' },
+            { name:'value', type:'object', required:false, defaultValue:{}, note:'data value' }
+        ];
+        return attributes;
+    },
+    
+    getItemLinks: function(linkItems) {
+        var linkElements = [];
+        if (linkItems) {
+            for (var i = 0; i < linkItems.length; i++) {
+                var linkItem = linkItems[i];
+                var linkItemKey = 'link-item-' + linkItem.text;
+                linkElements.push(
+                    <li className="link-container" key={ linkItemKey }>
+                        <a href={ linkItem.link }>{ linkItem.text }</a>
+                    </li>
+                );
+            }
+        }
+        return linkElements;
+    },
+    
+    render: function() {
+        var primaryLinkElements = this.getItemLinks( this.state.value.primaryLinks);
+        var secondaryLinkElements = this.getItemLinks( this.state.value.secondaryLinks);
+        var tertiaryLinkElements = this.getItemLinks( this.state.value.tertiaryLinks );
+        return (
+            <div className="fish_footer_container container">
+            <h2>FishFooter LLC</h2>
+                <div className="footer-links-primary col-md-3 footer-links">
+                    <ul className="list-unstyled">{ primaryLinkElements }</ul>
+                </div>
+                <div className="footer-links-secondary col-md-3 footer-links">
+                    <ul className="list-unstyled">{ secondaryLinkElements }</ul>
+                </div>
+                <div className="footer-links-tertiary col-md-3 footer-links">
+                    <ul className="list-unstyled">{ tertiaryLinkElements }</ul>
+                </div>
+            </div>
+        );
+    }
+});
