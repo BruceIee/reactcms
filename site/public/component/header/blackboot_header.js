5cfe1a6588675683d19efec12d2d36399850b83f
leapon <uestudio@gmail.com>
2015-08-28 22:25:40 -0400

    update field notes in blackboot header component

--- site/public/component/header/blackboot_header.js
+++ site/public/component/header/blackboot_header.js
@@ -9,9 +9,9 @@ var BlackbootHeader = React.createClass({
             { name:'boxClass', type:'string', required:false, defaultValue:'', note:'container CSS class' },
             { name:'iconClass', type:'string', required:false, defaultValue:'', note:'icon CSS class' },
             { name:'title', type:'string', required:false, defaultValue:'', note:'title' },
-            { name:'links1', type:'array', required:false, defaultValue:[], note:'main links' },
-            { name:'links2', type:'array', required:false, defaultValue:[], note:'side links' },
-            { name:'links3', type:'array', required:false, defaultValue:[], note:'extra links, not in use' }
+            { name:'links1', type:'array', required:false, defaultValue:[], note:'primary links' },
+            { name:'links2', type:'array', required:false, defaultValue:[], note:'secondary links' },
+            { name:'links3', type:'array', required:false, defaultValue:[], note:'tertiary links, not in use' }
         ];
         return attributes;
     },
