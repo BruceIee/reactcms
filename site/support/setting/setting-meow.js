bb10d23acc97c31a025fcb4cace81ba379d8607c
leapon <leapon@gmail.com>
2015-09-15 08:19:58 -0400

    setting-meow.js added in support/setting

---
+++ site/support/setting/setting-meow.js
@@ -0,0 +1,48 @@
+exports.setting = {
+
+    // web app
+    app_name: 'ReactCMS',
+    website: 'http://localhost',
+    access_check: false,
+    access_code: 'password',
+
+    // http/https mode
+    http_mode: true,
+    http_port: 8700,
+    https_mode: false,
+    https_port: 8443,
+    https_key: '',
+    https_certificate: '',
+    https_root_certificate: '',
+    https_intermediate_certificate: '',
+    http_to_https: false,  // redirect http to https
+
+    // database
+    database: {
+        type: 'mongo',
+        host: 'localhost',
+        port: 27017,
+        name: 'reactcms'
+    },
+    
+    // email setting
+    email: {
+        service: 'gmail',
+        username: 'test@example.com',
+        password: 'test',
+        sender: 'test@example.com'
+    },
+    
+    // site setting
+    site: {
+        homepage: 'meow_home',
+        header: 'meow_header',
+        footer: 'meow_footer'
+    },
+    
+    // invite code
+    invite: {
+        code: 'react_cms'
+    }
+
+};
