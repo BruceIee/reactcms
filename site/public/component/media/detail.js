947a877f631c9057308fdc0dcb379747b32d992d
leapon <leapon@gmail.com>
2015-09-17 08:32:35 -0400

    link added to media module; detail component updated

--- site/public/component/media/detail.js
+++ site/public/component/media/detail.js
@@ -12,6 +12,7 @@ var MediaDetail = React.createClass({
             { name:'name', type:'string', required:false, defaultValue:'', note:'item name' },
             { name:'type', type:'string', required:false, defaultValue:'', note:'item type' },
             { name:'description', type:'string', required:false, defaultValue:'', note:'item title' },
+            { name:'link', type:'string', required:false, defaultValue:'', note:'item web link' },
             { name:'image', type:'object', required:false, defaultValue:null, note:'image' },
             { name:'video', type:'object', required:false, defaultValue:null, note:'video' }
         ];
@@ -19,32 +20,34 @@ var MediaDetail = React.createClass({
     },
     
     render: function() {
-        var content = '';
+        var mediaContent = '';
         if (this.state.image) {
             var mediaUrl = '/file/' + this.state.image.filename;
-            var mediaContent = <img src={ mediaUrl} />;
-            content = 
-                <div className="container mediadetail-content">
-                    <div className="mediadetail-image-content">
-                        { mediaContent }
-                    </div>
+            mediaContent =
+                <div className="mediadetail-image-content">
+                    <img src={ mediaUrl} />
                 </div>;
         } else if (this.state.video) {
             var mediaUrl = '/file/' + this.state.image.filename;
-            var mediaContent = <video><source src={ mediaUrl} type="video/mp4" /></video>;
-            content = 
-                <div className="container mediadetail-content">
-                    <div className="mediadetail-video-content">
-                        { mediaContent }
-                    </div>
+            mediaContent =
+                <div className="mediadetail-video-content">
+                    <video>
+                        <source src={ mediaUrl} type="video/mp4" />
+                    </video>
                 </div>;
         }
-        
-        return (
+        if (this.state.link) {
+            mediaContent =
+                <a href={ this.state.link }>
+                    { mediaContent }
+                </a>
+        }
+        return
             <div className={ this.state.containerClassNames.join(' ') } data-id={ this.state.id } >
-                { content }
-            </div>
-        );
+                <div className="container mediadetail-content">
+                    { mediaContent }
+                </div>
+            </div>;
     }
 });
 
