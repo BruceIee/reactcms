fac2c710a94968b24d1d9a563c46f353fd6a34db
leapon <leapon@gmail.com>
2015-09-25 08:19:17 -0400

    update style on article detail component; iframe component cleanup

--- site/public/component/embed/iframe.js
+++ site/public/component/embed/iframe.js
@@ -19,44 +19,8 @@ var EmbedIframe = React.createClass({
     },
     
     render: function() {
-        var content = '';
-        
-        // https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23cccccc&amp;src=c2m9752m30fjqqkuk3lp7pl1k0%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FNew_York"
-        // scrolling="no" frameborder="0"
-        
-        content =
-            <iframe width="800" height="675"  src={ this.state.link } ></iframe>;
-
-        /*
-        if (this.state.image) {
-            var imageContent = '';
-            var imageUrl = '/file/' + this.state.image.filename;
-            if (this.state.image_link) {
-                imageContent =
-                    <a href={ this.state.image_link } >
-                        <img src={ imageUrl} style={ this.state.image_style } />
-                    </a>;
-            } else {
-                imageContent =
-                    <img src={ imageUrl} style={ this.state.image_style } />;
-            }
-            content = 
-                <div className="container articledetail-content">
-                    <div className="articledetail-image-content col-md-4">
-                        { imageContent }
-                    </div>
-                    <div className="articledetail-text-content col-md-8"
-                        dangerouslySetInnerHTML={{__html: this.state.content}}
-                    />
-                </div>
-        } else {
-            content = 
-                <div className="container articledetail-content"
-                    dangerouslySetInnerHTML={{__html: this.state.content}}
-                />
-        }
-        */
-        
+        var content = 
+            <iframe width="100%" height="675"  src={ this.state.link } style={ this.state.style } ></iframe>;
         return (
             <div className={ this.state.containerClassNames.join(' ') } data-id={ this.state.id } >
                 { content }
