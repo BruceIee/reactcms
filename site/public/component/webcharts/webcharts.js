af722311475a56c732bf001432994b4e498fb70b
leapon <leapon@gmail.com>
2015-10-10 17:30:16 -0400

    webchart component update

--- site/public/component/webcharts/webcharts.js
+++ site/public/component/webcharts/webcharts.js
@@ -1,7 +1,7 @@
 // WebChartss components
 // https://github.com/RhoInc/Webcharts
-var GraphBartest = React.createClass({
-    name: 'graph-bartest',
+var Webcharts = React.createClass({
+    name: 'webchart-bartest',
     mixins: [getCommonMixin],
     
     // attribute definitions
@@ -18,26 +18,113 @@ var GraphBartest = React.createClass({
     
     componentDidMount: function() {
         var graphElement = React.findDOMNode(this.refs.graph);
-        // plot using d3
-        var graphData = this.state.value;
-        var x = d3.scale.linear()
-            .domain([0, d3.max(graphData)])
-            .range([0, 420]);
-        d3.select(graphElement)
-            .selectAll('div')
-            .data(graphData)
-            .enter().append('div')
-            .style('width', function(d) { return x(d) + 'px'; })
-            .text(function(d) { return d; });
+        
+        var settings = {
+            "max_width":"500",
+            "x":{
+              "label":"Protein (g)",
+              "type":"linear",
+              "column":"Protein (g)"
+            },
+            "y":{
+              "label":"Carbs (g)",
+              "type":"linear",
+              "column":"Carbo(g)"
+            },
+            "marks":[
+              {
+                "type":"circle",
+                "per":["Food"],
+                "tooltip":"[Food]\n[Measure]\n[Protein (g)] grams protein\n[Carbo(g)] grams carbs"
+              }
+            ],
+            "aspect":"1",
+            "gridlines":"xy"
+        };
+        
+        var calChart = webCharts.createChart(graphElement, settings);
+        d3.csv('/data/webcharts/calories.csv', function(error, data){
+            calChart.init(data);
+        });
+            
     },
     
     render: function() {
         return (
             <div className={ this.state.containerClassNames.join(' ') } data-id={ this.state.id } >
-                <div className="graph-bartest-content">
-                    <div className="reactlet-graph" ref="graph"></div>
+                <div className="webcharts-content">
+                    <div className="webcharts-graph" ref="graph"></div>
                 </div>
             </div>
         );
     }
 });
+
+
+/*
+<script src="/lib/d3cdn/d3.v3.min.js"></script>
+<script src="/lib/webcharts/webcharts.hole.js"></script>
+
+    // scatter plot
+    var settings = {
+        "max_width":"500",
+        "x":{
+          "label":"Protein (g)",
+          "type":"linear",
+          "column":"Protein (g)"
+        },
+        "y":{
+          "label":"Carbs (g)",
+          "type":"linear",
+          "column":"Carbo(g)"
+        },
+        "marks":[
+          {
+            "type":"circle",
+            "per":["Food"],
+            "tooltip":"[Food]\n[Measure]\n[Protein (g)] grams protein\n[Carbo(g)] grams carbs"
+          }
+        ],
+        "aspect":"1",
+        "gridlines":"xy"
+      };
+      
+    var calChart = webCharts.createChart('.graphic-wrapper', settings);
+    d3.csv('/data/webcharts/calories.csv', function(error, data){
+        calChart.init(data);
+    });
+
+    
+    // barchart
+    var settings = {
+      "max_width":"500",
+      "x":{
+        "label":"Total",
+        "type":"linear",
+        "column":"Total",
+        domain: [0, null]
+      },
+      "y":{
+        "type":"ordinal",
+        "column":"Country",
+        "sort":"total-descending"
+      },
+      "marks":[
+        {
+          "type":"bar",
+          "per":["Country"],
+          "tooltip":"[Country] won [Total] medals"
+        }
+      ],
+      "gridlines":"x"
+    };
+    
+    var medalChart = webCharts.createChart('.graphic-wrapper', settings);
+    d3.csv('/data/webcharts/OlympicMedals2012.csv', function(error, data){
+      //just keep the countries with the 10 most medals 
+      var sub = data.filter(function(d,i){
+        return i <= 11;
+      });
+      medalChart.init(sub);
+    });
+*/
