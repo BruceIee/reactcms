0e4e12aa342150b3262827cbb2cde36beeff19db
leapon <leapon@gmail.com>
2015-10-14 08:18:55 -0400

    added manage page for user manager

--- site/app_modules/admin.js
+++ site/app_modules/admin.js
@@ -11,17 +11,27 @@ module.exports = function(app) {
     block.data = tool.object(require('basedata')(app, moduleName));
     block.page = tool.object(require('basepage')(app, moduleName, block.data));
     
-    // page
-    block.page.getIndex = function(req, res) {
+    // site admin page
+    block.page.getAdminPage = function(req, res) {
         var page = app.getPage(req);
         res.render('admin/index', { page:page });
+    };
+    
+    // user manager page
+    block.page.getManagePage = function(req, res) {
+        var page = app.getPage(req);
+        res.render('admin/manage', { page:page });
     };
     
     // routes
     app.server.all('/admin', block.page.checkLogin);
     app.server.all('/admin/*', block.page.checkLogin);
-    app.server.get('/admin', block.page.getIndex);
+    app.server.get('/admin', block.page.getAdminPage);
     app.server.get('/' + moduleName + '/page/:pagename', block.page.showPage);
+    
+    app.server.all('/manage', block.page.checkLogin);
+    app.server.all('/manage/*', block.page.checkLogin);
+    app.server.get('/manage', block.page.getManagePage);
     
     return block;
 };
