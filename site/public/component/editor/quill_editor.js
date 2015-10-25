4db907336c5d4b4db7622c94180254fb2345714e
leapon <leapon@gmail.com>
2015-10-25 16:51:13 -0400

    quill editor update

--- site/public/component/editor/quill_editor.js
+++ site/public/component/editor/quill_editor.js
@@ -9,8 +9,8 @@ var QuillEditor = React.createClass({
             { name:'boxClass', type:'string', required:false, defaultValue:'', note:'container CSS class' },
             { name:'iconClass', type:'string', required:false, defaultValue:'', note:'icon CSS class' },
             { name:'type', type:'string', required:false, defaultValue:'', note:'item type' },
-            { name:'theme', type:'string', required:false, defaultValue:'basic', note:'editor theme' },
-            { name:'readOnly', type:'boolean', required:false, defaultValue:true, note:'set editor read only' },
+            { name:'theme', type:'string', required:false, defaultValue:'snow', note:'editor theme' },
+            { name:'readOnly', type:'boolean', required:false, defaultValue:false, note:'set editor read only' },
             { name:'title', type:'string', required:false, defaultValue:'', note:'item title' },
             { name:'value', type:'string', required:false, defaultValue:'', note:'item value' }
         ];
@@ -21,17 +21,22 @@ var QuillEditor = React.createClass({
         var editorElement = React.findDOMNode(this.refs.quilleditor);
         var editorToolbarElement = React.findDOMNode(this.refs.quilleditor_toolbar);
         // create editor object
-        var editor = new Quill(editorElement);
-        editor.addModule('toolbar', { container: editorToolbarElement });
+        var configs = {
+            //readOnly: this.state.readOnly,
+            theme: this.state.theme
+        };
+        var editor = new Quill(editorElement, configs);
+        editor.addModule('toolbar', { container: '.quill-editor-toolbar' });
+        editor.setText(this.state.value || '');
     },
     
     render: function() {
         return (
             <div className={ this.state.containerClassNames.join(' ') } data-id={ this.state.id } >
                 <div className="quill-editor-content">
-                    <div className="quill-editor-toolbar" id="quilleditor_toolbar">
-                        <button class="ql-bold">Bold</button>
-                        <button class="ql-italic">Italic</button>
+                    <div className="quill-editor-toolbar sc-toolbar-container toolbar">
+                        <button className="ql-bold">Bold</button>
+                        <button className="ql-italic">Italic</button>
                     </div>
                     <div className="quill-editor" ref="quilleditor">
                     </div>
